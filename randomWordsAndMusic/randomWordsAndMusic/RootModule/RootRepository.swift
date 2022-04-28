ef1cedeba667ebf3cdb3dc05ebb248b5175dd1c0
Pavel <pavel.kurzo@mail.ru>
2022-04-28 13:04:13 +0200

    Creation of RootRepository

---
+++ randomWordsAndMusic/randomWordsAndMusic/RootModule/RootRepository.swift
@@ -0,0 +1,29 @@
+//
+//  RootRepository.swift
+//  randomWordsAndMusic
+//
+//  Created by Павел Курзо on 28.04.22.
+//
+
+import Foundation
+
+class RootRepository {
+    
+    private let musicFetcher: MusicFetching
+    private let wordsFetcher: WordsFetching
+
+    init(musicFetcher: MusicFetching = MusicFetcher(),
+         wordsFetcher: WordsFetching = WordsFetcher()) {
+        self.musicFetcher = musicFetcher
+        self.wordsFetcher = wordsFetcher
+    }
+
+    func fetchWords(_ number: Int, completion: @escaping (Result<[WordsResult], Error>) -> Void) {
+        wordsFetcher.fetchWords(number, completion: completion)
+    }
+
+    func fetchMusic(for words: [WordsResult], completion: ((Result<[MusicData], Error>) -> Void)?) {
+        musicFetcher.fetchMusic(for: words, completion: completion)
+    }
+
+}
