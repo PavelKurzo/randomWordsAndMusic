30769bd5e994f423e84880f5c604e91eeaa1ef0d
Pavel <pavel.kurzo@mail.ru>
2022-04-28 12:33:43 +0200

    Deleted main storyBoard

---
+++ randomWordsAndMusic/randomWordsAndMusic/RootModule/RootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  randomWordsAndMusic
+//
+//  Created by Павел Курзо on 28.04.22.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
+
